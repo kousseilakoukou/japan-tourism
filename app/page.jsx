8f2bfd1affff1c3ddd8c9ef36dca8acf87673673
kousseilakoukou <Igguikousseila4@gmail.com>
2025-05-15 22:58:39 -0400

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+export default function Home() {
+    return (
+        <main className="flex justify-center items-center h-screen">
+        <div className="text-blue-700">
+            Bienvenue au Japon
+        </div>
+        </main>
+    );
+}
